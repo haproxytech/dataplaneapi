ef9b519e76b684cd55f577711dc95c51d5643042
Marko Juraga <mjuraga@haproxy.com>
2022-02-22 10:32:15 +0100

    DOC: specification: update specification to proper version

--- doc.go
+++ doc.go
@@ -23,7 +23,7 @@
 //    http
 //  Host: localhost
 //  BasePath: /v2
-//  Version: 2.4
+//  Version: 2.5
 //  Contact: HAProxy Support<support@haproxy.com> https://my.haproxy.com/portal/cust/login
 //
 //  Consumes:
