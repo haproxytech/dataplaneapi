ddad8628cfec3b1e9dfe99ebecfed8239153cb71
Marko Juraga <mjuraga@haproxy.com>
2022-10-20 12:58:41 +0200

    TEST/MINOR: e2e: fetch hapropxy version from env variable, more reliable

--- e2e/libs/haproxy_version.bash
+++ e2e/libs/haproxy_version.bash
@@ -15,24 +15,6 @@
 # limitations under the License.
 #
 
-#load '../../libs/debug'
-
-# haproxy_version curls the /runtime/info endpoint to get the current
-# haproxy version and returns it as a string. haproxy must be setup with a
-# valid stats socket for this to work, and it uses deprecated_auth_curl
-# from version.bash script.
-#
-# Example:
-# haproxy_version
-# >>> 2.2.18-c6e1dfa
-function haproxy_version() {
-    read -r SC RES < <(deprecated_auth_curl GET "/v2/services/haproxy/runtime/info")
-    V="$(get_json_path "$RES" ".[0].info.version")"
-    #debug $RES
-    #debug $V
-    echo "$V"
-}
-
 # haproxy_version_ge returns 1 if the haproxy version is greater or
 # equal to the given version, otherwise 0. Given version must be in format
 # MAJOR.MINOR without the patch and commit numbers (e.g. 1.9, 2.2, etc.).
@@ -45,8 +27,7 @@ function haproxy_version() {
 # >>> 1
 function haproxy_version_ge() {
     target=$1; shift
-    v=$(haproxy_version)
-    major_minor="${v%.*}"
+    major_minor=$HAPROXY_VERSION
     numerical_v="${major_minor//.}"
     numerical_target="${target//.}"
 
