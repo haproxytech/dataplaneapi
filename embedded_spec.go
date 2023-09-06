5eac4699016a3c795c15706fd1fcd7de62f0ef32
Marko Juraga <mjuraga@haproxy.com>
2023-09-06 11:09:38 +0200

    BUILD/MINOR: client-native: upgrade client-native version

--- embedded_spec.go
+++ embedded_spec.go
@@ -8674,6 +8674,14 @@ func init() {
               }
             },
             "headers": {
+              "Cluster-Version": {
+                "type": "string",
+                "description": "Cluster configuration version"
+              },
+              "Configuration-Checksum": {
+                "type": "string",
+                "description": "Configuration file md5 checksum"
+              },
               "Configuration-Version": {
                 "type": "string",
                 "description": "Configuration file version"
@@ -8746,6 +8754,20 @@ func init() {
             "description": "New HAProxy configuration pushed",
             "schema": {
               "type": "string"
+            },
+            "headers": {
+              "Cluster-Version": {
+                "type": "string",
+                "description": "Cluster configuration version"
+              },
+              "Configuration-Checksum": {
+                "type": "string",
+                "description": "Configuration file md5 checksum"
+              },
+              "Configuration-Version": {
+                "type": "string",
+                "description": "Configuration file version"
+              }
             }
           },
           "202": {
@@ -8754,6 +8776,18 @@ func init() {
               "type": "string"
             },
             "headers": {
+              "Cluster-Version": {
+                "type": "string",
+                "description": "Cluster configuration version"
+              },
+              "Configuration-Checksum": {
+                "type": "string",
+                "description": "Configuration file md5 checksum"
+              },
+              "Configuration-Version": {
+                "type": "string",
+                "description": "Configuration file version"
+              },
               "Reload-ID": {
                 "type": "string",
                 "description": "ID of the requested reload"
@@ -16149,8 +16183,7 @@ func init() {
       "required": [
         "index",
         "acl_name",
-        "criterion",
-        "value"
+        "criterion"
       ],
       "properties": {
         "acl_name": {
@@ -16591,6 +16624,7 @@ func init() {
               "value": "http"
             }
           },
+          "x-deprecated": true,
           "x-display-name": "HTTP keep-alive"
         },
         "http-no-delay": {
@@ -16617,6 +16651,7 @@ func init() {
               "value": "http"
             }
           },
+          "x-deprecated": true,
           "x-display-name": "HTTP server close"
         },
         "http-use-htx": {
@@ -16738,6 +16773,7 @@ func init() {
               "value": "http"
             }
           },
+          "x-deprecated": true,
           "x-display-name": "HTTP connection closing"
         },
         "id": {
@@ -42480,6 +42516,14 @@ func init() {
               }
             },
             "headers": {
+              "Cluster-Version": {
+                "type": "string",
+                "description": "Cluster configuration version"
+              },
+              "Configuration-Checksum": {
+                "type": "string",
+                "description": "Configuration file md5 checksum"
+              },
               "Configuration-Version": {
                 "type": "string",
                 "description": "Configuration file version"
@@ -42569,6 +42613,20 @@ func init() {
             "description": "New HAProxy configuration pushed",
             "schema": {
               "type": "string"
+            },
+            "headers": {
+              "Cluster-Version": {
+                "type": "string",
+                "description": "Cluster configuration version"
+              },
+              "Configuration-Checksum": {
+                "type": "string",
+                "description": "Configuration file md5 checksum"
+              },
+              "Configuration-Version": {
+                "type": "string",
+                "description": "Configuration file version"
+              }
             }
           },
           "202": {
@@ -42577,6 +42635,18 @@ func init() {
               "type": "string"
             },
             "headers": {
+              "Cluster-Version": {
+                "type": "string",
+                "description": "Cluster configuration version"
+              },
+              "Configuration-Checksum": {
+                "type": "string",
+                "description": "Configuration file md5 checksum"
+              },
+              "Configuration-Version": {
+                "type": "string",
+                "description": "Configuration file version"
+              },
               "Reload-ID": {
                 "type": "string",
                 "description": "ID of the requested reload"
@@ -54644,8 +54714,7 @@ func init() {
       "required": [
         "index",
         "acl_name",
-        "criterion",
-        "value"
+        "criterion"
       ],
       "properties": {
         "acl_name": {
@@ -55086,6 +55155,7 @@ func init() {
               "value": "http"
             }
           },
+          "x-deprecated": true,
           "x-display-name": "HTTP keep-alive"
         },
         "http-no-delay": {
@@ -55112,6 +55182,7 @@ func init() {
               "value": "http"
             }
           },
+          "x-deprecated": true,
           "x-display-name": "HTTP server close"
         },
         "http-use-htx": {
@@ -55233,6 +55304,7 @@ func init() {
               "value": "http"
             }
           },
+          "x-deprecated": true,
           "x-display-name": "HTTP connection closing"
         },
         "id": {
