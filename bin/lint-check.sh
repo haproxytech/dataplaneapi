b184a4ea3247cff9f3a37193b51f9976190ee39f
Marko Juraga <mjuraga@haproxy.com>
2026-09-03 14:28:14 +0200

    BUILD/MEDIUM: go: upgrade go to 1.27

--- bin/lint-check.sh
+++ bin/lint-check.sh
@@ -3,5 +3,5 @@ V=$(./golangci-lint --version)
 
 case "$V" in
   *$GOLANGCI_LINT_VERSION*) echo "$V" ;;
-  *)          curl -sSfL https://raw.githubusercontent.com/golangci/golangci-lint/master/install.sh | sh -s -- -b $(pwd) "v$GOLANGCI_LINT_VERSION" ;;
+  *)          curl -sSfL https://raw.githubusercontent.com/golangci/golangci-lint/refs/heads/main/install.sh | sh -s -- -b $(pwd) "v$GOLANGCI_LINT_VERSION" ;;
 esac
